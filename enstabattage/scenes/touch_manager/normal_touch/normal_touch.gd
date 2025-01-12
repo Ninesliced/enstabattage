b3c3db9002bc1ddd694d20041c75c7d5bb62e7d7
Gouspourd <71386539+Gouspourd@users.noreply.github.com>
2025-01-12 21:35:32 +0100

    knockback with shot

--- enstabattage/scenes/touch_manager/normal_touch/normal_touch.gd
+++ enstabattage/scenes/touch_manager/normal_touch/normal_touch.gd
@@ -6,6 +6,8 @@ class_name Touch
 @export var damage = 1
 @export var coodown_time = .2
 @export var is_auto = false
+@export var knockback_amount = 50
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,7 +23,7 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is LivingEntity and body.is_touchable:
 		body.deal_damage(self, damage)
-
+		body.deal_knockback(Vector2(rng.randf_range(-1, 1), -1).normalized(), knockback_amount)
 
 func _on_start_timer_timeout() -> void:
 	flash.show()
