5c4e88a197472a394075196dede0d172fb149d3b
Gouspourd <71386539+Gouspourd@users.noreply.github.com>
2025-01-15 23:55:12 +0100

    round_manager_plus_permier_rouds_et_premier_prix_darmes

--- enstabattage/scenes/spawner/spawner.gd
+++ enstabattage/scenes/spawner/spawner.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export_file("*.tscn") var entity_path: String
-@export var spawn_radius: float = 10
+@export var spawn_radius: float = 10.0
 @export_range(-360, 360, 0.1, "radians_as_degrees") var minimum_angle: float = PI/2 - PI/4
 @export_range(-360, 360, 0.1, "radians_as_degrees") var maximum_angle: float = PI/2 + PI/4
 @export_range(0, 135, 0.1) var x_pos: float = 67.5
@@ -9,8 +9,8 @@ extends Node2D
 @export var maximum_velocity = 20
 @export var minimum_delay = 0.5
 @export var maximum_delay = 1.0
-@export var time_before_spawning_start = 10
-@export var spawn_duration = 10
+@export var time_before_spawning_start = 10.0
+@export var spawn_duration = 10.0
 
 @onready var spawn_timer: Timer = $SpawnTimer
 @onready var spawn_delay: Timer = $SpawnDelay
@@ -24,7 +24,7 @@ func _ready():
 	entity_file = load(entity_path)
 	spawn_delay.wait_time = time_before_spawning_start
 	span_timer.wait_time = spawn_duration
-	position = Vector2(x_pos,0)
+	position = Vector2(x_pos,-10)
 
 func _spawn_entity():
 	var entity = entity_file.instantiate()
@@ -32,9 +32,9 @@ func _spawn_entity():
 	var angle = rng.randf_range(minimum_angle, maximum_angle)
 	var vel = rng.randf_range(minimum_velocity, maximum_velocity)
 	print(pos)
-	entity.position = pos
+	entity.global_position = pos + position
 	entity.velocity = Vector2(vel, 0).rotated(angle)
-	add_child(entity)
+	get_parent().add_child(entity)
 
 func _on_spawn_timer_timeout():
 	_spawn_entity()
@@ -44,7 +44,6 @@ func _init_timer():
 	spawn_timer.wait_time = rng.randf_range(minimum_delay, maximum_delay)
 	spawn_timer.start()
 
-
 func _on_spawn_delay_timeout() -> void:
 	_init_timer()
 	span_timer.start()
@@ -53,7 +52,6 @@ func _on_spawn_delay_timeout() -> void:
 func start_round():
 	spawn_delay.start()
 
-
 func _on_span_timer_timeout() -> void:
-	#queue_free()
+	queue_free()
 	pass # Replace with function body.
