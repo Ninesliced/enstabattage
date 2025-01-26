a7d0e9a18b0b3c7d122117304b8205317567e075
Yolwoocle <leo.bernard2015@hotmail.com>
2025-01-26 18:29:23 +0100

    Screenshake

--- enstabattage/scenes/camera/camera.gd
+++ enstabattage/scenes/camera/camera.gd
@@ -1,7 +1,28 @@
 extends Camera2D
+class_name Camera
+
+var shake_amount = 0.0
+var shake_decrease_speed = 6.0
+
+var _rng = RandomNumberGenerator.new()
+
 
 func _ready():
-    position = get_viewport().size / 2
+    pass
+
 
 func _process(delta):
-    pass
+    _update_shake(delta)
+
+    if Input.is_action_just_pressed("ui_accept"):
+        shake(10)
+
+
+func _update_shake(delta):
+    shake_amount = max(0.0, shake_amount - delta * shake_decrease_speed)
+
+    offset = Vector2(_rng.randf_range(0, shake_amount), 0).rotated(_rng.randf_range(0, TAU)).round()
+
+
+func shake(value):
+    shake_amount = max(shake_amount, value)
