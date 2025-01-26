f6e85980a59c2233a9a2294bdb4f9e2e7530431f
Yolwoocle <leo.bernard2015@hotmail.com>
2025-01-27 00:02:54 +0100

    Volume fix

--- enstabattage/scenes/ui/options_menu.gd
+++ enstabattage/scenes/ui/options_menu.gd
@@ -2,7 +2,9 @@ extends Menu
 
 
 func _process(delta):	
-	pass
+	var volume_val = %VolumeSlider.value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(volume_val))
+
 
 func _on_back_pressed():
 	Global.menu_manager.back()
@@ -17,4 +19,3 @@ func _on_change_list_pressed():
 
 func _on_reset_name_pressed():
 	Leaderboard.reset_name()
-
